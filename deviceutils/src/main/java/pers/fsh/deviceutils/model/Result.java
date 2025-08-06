a87c1732ed962fd83ae4424c32e594894ddaf266
fanshuhua <fanshuhua@yftech.com>
2025-08-06 15:32:52 +0800

    feat: 更新硬件返回数据对象，增加success参数

--- deviceutils/src/main/java/pers/fsh/deviceutils/model/Result.java
+++ deviceutils/src/main/java/pers/fsh/deviceutils/model/Result.java
@@ -1,86 +1,69 @@
 package pers.fsh.deviceutils.model;
 
 import lombok.Getter;
-import lombok.Setter;
+
+import java.io.Serializable;
 
 /**
+ * 统一返回结果类
+ *
  * @author fanshuhua
  * @date 2025/5/19 19:18
  */
-@Setter
 @Getter
-public class Result<T> {
-    private int code;
-    private String message;
-    private T data;
+public class Result<T> implements Serializable {
 
-    public Result(int code, String message, T data) {
+    private static final long serialVersionUID = 1L;
+
+    private final boolean success;
+    private final int code;
+    private final String message;
+    private final T data;
+
+    private Result(boolean success, int code, String message, T data) {
+        this.success = success;
         this.code = code;
         this.message = message;
         this.data = data;
     }
 
-    public Result(int code, String message) {
-        this.code = code;
-        this.message = message;
-    }
-
-    public Result() {
-    }
-
-    public static Result<String> success() {
-        return new Result<>(1, "success");
+    public static <T> Result<T> success() {
+        return new Result<>(true, 1, "success", null);
     }
 
     public static <T> Result<T> success(T data) {
-        return new Result<>(1, "success", data);
+        return new Result<>(true, 1, "success", data);
     }
 
-    public static Result<String> success(String msg) {
-        return new Result<>(1, msg);
+    public static <T> Result<T> success(String message) {
+        return new Result<>(true, 1, message, null);
     }
 
-    public static Result<String> success(int code, String msg) {
-        return new Result<>(code, msg);
+    public static <T> Result<T> success(String message, T data) {
+        return new Result<>(true, 1, message, data);
     }
 
-    public static <T> Result<T> success(String msg, T data) {
-        return new Result<>(1, msg, data);
+    public static <T> Result<T> error() {
+        return new Result<>(false, -1, "error", null);
     }
 
-    public static <T> Result<T> success(int code, String msg, T data) {
-        return new Result<>(code, msg, data);
+    public static <T> Result<T> error(String message) {
+        return new Result<>(false, -1, message, null);
     }
 
-
-    public static Result<String> error() {
-        return new Result<>(-1, "error");
+    public static <T> Result<T> error(int code, String message) {
+        return new Result<>(false, code, message, null);
     }
 
-    public static Result<String> error(String msg) {
-        return new Result<>(-1, msg);
-    }
-
-    public static <T> Result<T> error(T data) {
-        return new Result<>(-1, "error", data);
-    }
-
-    public static Result<String> error(int code, String msg) {
-        return new Result<>(code, msg);
-    }
-
-    public static <T> Result<T> error(String msg, T data) {
-        return new Result<>(-1, msg, data);
-    }
-
-    public static <T> Result<T> error(int code, String msg, T data) {
-        return new Result<>(code, msg, data);
+    public static <T> Result<T> error(int code, String message, T data) {
+        return new Result<>(false, code, message, data);
     }
 
     @Override
     public String toString() {
         return "Result{" +
-                "code=" + code +
+                "success=" + success +
+                ", code=" + code +
                 ", message='" + message + '\'' +
                 ", data=" + data +
                 '}';
